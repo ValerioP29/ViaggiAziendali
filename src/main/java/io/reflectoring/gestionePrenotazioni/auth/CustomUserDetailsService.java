172900d7466370684990b98e15435058073de8a9
Valerio Persiani <persianivalerio0@gmail.com>
2025-01-14 16:11:49 +0100

    esercizio giorno 2

--- src/main/java/io/reflectoring/gestionePrenotazioni/auth/CustomUserDetailsService.java
+++ src/main/java/io/reflectoring/gestionePrenotazioni/auth/CustomUserDetailsService.java
@@ -1,6 +1,5 @@
 package io.reflectoring.gestionePrenotazioni.auth;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
@@ -19,14 +18,16 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // Recupera l'utente dal database
         AppUser appUser = appUserRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("Utente non trovato con username: " + username));
 
+        // Mappa i ruoli dell'utente e aggiunge il prefisso "ROLE_" al ruolo
         return new User(
                 appUser.getUsername(),
                 appUser.getPassword(),
                 appUser.getRoles().stream()
-                        .map(role -> new SimpleGrantedAuthority(role.name()))
+                        .map(role -> new SimpleGrantedAuthority("ROLE_" + role.name()))  // Aggiungi "ROLE_" ai ruoli
                         .collect(Collectors.toList())
         );
     }
